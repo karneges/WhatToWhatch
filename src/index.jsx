8f1467304a0574770178d91634d149b42fa1bba9
Nikita <karneges2012@gmail.com>
2020-02-09 12:54:20 +0200

    fix routes bugs

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./routes";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { FilmContextProvider } from "./contexts/film-context-service";
 
 const App = () => {
