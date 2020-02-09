2705969ce620c8c56e700d00d3499817bb81f0f4
Nikita <karneges2012@gmail.com>
2020-02-09 12:45:58 +0200

    Updates

--- precache-manifest.7041da0d100435f08cd3f45f6b9121e6.js
+++ precache-manifest.7041da0d100435f08cd3f45f6b9121e6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4b9bdfa5641e3263d7b1c1e24aa57063",
+    "revision": "80fa69bc9129af0321da0490ed9f8b2f",
     "url": "/karneges.github.io/wtw/index.html"
   },
   {
-    "revision": "3a5f09e7ef6d217dc874",
+    "revision": "62db644553a9d75a7bc0",
     "url": "/karneges.github.io/wtw/static/css/main.01be1241.chunk.css"
   },
   {
-    "revision": "c87c4ad222abaa346dbc",
-    "url": "/karneges.github.io/wtw/static/js/2.8aaaa013.chunk.js"
+    "revision": "e0ac220d72ced20c3186",
+    "url": "/karneges.github.io/wtw/static/js/2.2d4bbf6d.chunk.js"
   },
   {
     "revision": "86afaa925e0f432774bf95b19b6933fb",
-    "url": "/karneges.github.io/wtw/static/js/2.8aaaa013.chunk.js.LICENSE"
+    "url": "/karneges.github.io/wtw/static/js/2.2d4bbf6d.chunk.js.LICENSE"
   },
   {
-    "revision": "3a5f09e7ef6d217dc874",
-    "url": "/karneges.github.io/wtw/static/js/main.4e28b123.chunk.js"
+    "revision": "62db644553a9d75a7bc0",
+    "url": "/karneges.github.io/wtw/static/js/main.00aec70e.chunk.js"
   },
   {
     "revision": "ff871e0a373c800db48e",
